2a83e0422571e70d1357bdcb4d9c67d0bf55c467
xiejava <shygiant@126.com>
2014-11-04 00:26:07 +0800

    First commit

---
+++ src/com/doit8/sleep/SleepAppActivity.java
@@ -0,0 +1,13 @@
+package com.doit8.sleep;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SleepAppActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
