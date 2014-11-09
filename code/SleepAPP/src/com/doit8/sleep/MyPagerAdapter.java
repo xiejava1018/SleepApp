fa7ebba4aba4fbf3d89ae30505255443030361c6
xiejava <shygiant@126.com>
2014-11-09 14:20:24 +0800

    MyPagerAdapter 实现继承的方法

--- code/SleepAPP/src/com/doit8/sleep/MyPagerAdapter.java
+++ code/SleepAPP/src/com/doit8/sleep/MyPagerAdapter.java
@@ -1,6 +1,8 @@
 package com.doit8.sleep;
 
 import java.util.ArrayList;
+
+import android.os.Parcelable;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
@@ -29,5 +31,25 @@ public class MyPagerAdapter extends PagerAdapter{
 	public Object instantiateItem(View container, int position) {
 		((ViewPager)container).addView(views.get(position));
 		return views.get(position);
+	}
+	@Override
+	public void finishUpdate(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void restoreState(Parcelable arg0, ClassLoader arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public Parcelable saveState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void startUpdate(View arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
